9197d29f79bfb10242dae77c23438745b1ead137
ericpkatz <ericpkatz@gmail.com>
2022-10-29 19:14:14 -0400

    commit

--- src/Components/PromptAttempt.js
+++ src/Components/PromptAttempt.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { savePromptAttempt } from '../store';
 import { useDispatch } from 'react-redux';
+import { formatDate, executeCode, _logger } from '../utils';
 
 const PromptAttempt = ({ promptAttempt })=> {
   const [el, setEl] = useState(null);
   const [editor, setEditor] = useState(null);
+  const [ _console, setConsole] = useState(null);
   const dispatch = useDispatch();
 
   useEffect(()=> {
@@ -15,13 +17,22 @@ const PromptAttempt = ({ promptAttempt })=> {
         language: 'javascript'
       });
       setEditor(_editor);
+      _editor.on('change', ev => {
+        setAttempt(ev.getValue());
+        
+      });
     }
   }, [el]);
   const [attempt, setAttempt] = useState(promptAttempt.attempt || '');
 
+  const _executeCode = ()=> {
+    const logger = _logger(_console);
+    executeCode(editor.getValue(), logger, JSHINT);
+  }
+
   const save = ev => {
     ev.preventDefault();
-    console.log('we have the prompt? Can we assume the enrollment? Prompt, to topic. Is the topic currently assigned to an enrollment the cohort is in?');
+    _executeCode();
     promptAttempt = {...promptAttempt, attempt: editor.getValue(), submitted: document.activeElement.id === 'submit' ? true : false };
     dispatch(savePromptAttempt(promptAttempt));
   }
@@ -29,10 +40,13 @@ const PromptAttempt = ({ promptAttempt })=> {
   return (
     <div>
       <form onSubmit={ save }>
-        <div ref={el => setEl(el)}></div>
+        <div id='ide'>
+          <div ref={el => setEl(el)}></div>
+          <div className='console' ref={el => setConsole(el)}></div>
+        </div>
         <div>
-          <button id='run'>Run</button>
-          <button id='submit' disabled={ !promptAttempt.id }>Submit</button>
+          <button id='run' disabled={!attempt}>Run Your Code</button>
+          <button id='submit' disabled={ !promptAttempt.id }>Submit Your Code to Get Next Prompt</button>
         </div>
       </form>
     </div>
---
+++ src/utils.js
@@ -0,0 +1,146 @@
+const formatDate = ({ date, timeZone, showTimeZone = true })=> {
+  return moment(date).tz(timeZone).format(`MM/DD/YYYY h:mma${ showTimeZone ? ' z': ''}`); 
+}
+
+const criticalErrors = (code, JSHINT)=> {
+  JSHINT(code);
+  let errors = JSHINT.errors;
+  errors = errors.filter( e => e.code.startsWith('E')).map(e => new Error(e.reason));
+  if(errors.length){
+    return errors;
+  }
+}
+
+
+const _logger = (div)=> {
+  const fn = (...messages)=> {
+    if(messages[0] === null){
+      return div.innerHTML = '';
+    }
+    if(messages[0] instanceof Error){
+      div.innerHTML = '';
+    } 
+    //div.innerHTML = '';
+    messages.forEach( message => {
+      let _message = message;
+      if(!(message instanceof Error)){
+        _message = typeof message === 'object' ? `<pre>${JSON.stringify(message, null, 2)}</pre>` : message;
+      }
+      else {
+        _message = message.message
+      }
+      //if message is error, clear it out!
+    
+      if(div){
+        div.innerHTML += `<div ${ message instanceof Error ? 'style=color:red': ''}><span class='cursor'>&gt;&gt; </span>${_message}</div>`;
+      }
+    });
+  };
+  fn.div = div;
+  return fn;
+};
+
+const infiniteFree = (code)=>{
+  return new Promise((resolve, reject)=> {
+    limitEval(`let ex;try{${code}}catch(e){
+      ex = e;
+    };ex`, (...args)=> {
+      if(args.length === 2){
+        resolve();
+      }
+      else {
+        reject(new Error('infinite loop detected'));
+      }
+    }, 1000);
+
+  });
+};
+
+function limitEval(code, fnOnStop, opt_timeoutInMS) {
+  const start = new Date();
+  var id = Math.random() + 1,
+    blob = new Blob(
+      ['onmessage=function(a){a=a.data;postMessage({i:a.i+1});postMessage({r:eval.call(this,a.c),i:a.i})};'],
+      { type:'text/javascript' }
+    ),
+    myWorker = new Worker(URL.createObjectURL(blob));
+
+  function onDone() {
+    URL.revokeObjectURL(blob);
+    fnOnStop.apply(this, arguments);
+  }
+
+  myWorker.onmessage = function (data) {
+    data = data.data;
+    if (data) {
+      if (data.i === id) {
+        id = 0;
+        onDone(true, data.r);
+      }
+      else if (data.i === id + 1) {
+        setTimeout(function() {
+          if (id) {
+            myWorker.terminate();
+            onDone(false);
+          }
+        }, opt_timeoutInMS || 1000);
+      }
+    }
+  };
+
+  myWorker.postMessage({ c: code, i: id });
+}
+
+
+const executeCode = function(code, logger, JSHINT = window.JSHINT){
+  if(!code.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'').trim()){
+    return logger(null);
+  }
+  code = code.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'');
+  const _criticalErrors = criticalErrors(code, JSHINT);
+  if(_criticalErrors){
+    logger(..._criticalErrors);
+    return;
+
+  }
+  //check if there is any code to run!
+  infiniteFree(code)
+    .then(()=> runCode(code, logger))
+    .catch(ex => {
+      console.log(ex);
+      logger(ex);
+    });
+}
+
+const runCode = function(code, logger){
+  const _log = console.log;
+  console.log = logger;
+  //console.log = logger;
+  logger.div.innerHTML = '';
+  console.log();//?
+  try {
+    eval(code);
+    console.log = _log;
+  }
+  catch(ex){
+    console.log(ex);
+    console.log = _log;
+  }
+}
+
+
+const formProcessor = (ev) => {
+  function getBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+  }
+  const name = ev.target.files[0].name;
+  return getBase64(ev.target.files[0])
+    .then(data => ({ name, file: data }));
+};
+
+export { formatDate, formProcessor, executeCode, _logger };
